055a592761c9d5dc0afcf04efd8e910d49239167
Karena <k.mistry1898@gmail.com>
2022-11-11 17:52:16 -0500

    scroll top

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/footer";
 import Footerbar from "./components/footerbar";
+import ScrollToTop from "./scrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
       <Footerbar />
       <Footer />
