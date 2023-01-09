df3347e41971caf778faac738b46554f579bc9c5
Karena <k.mistry1898@gmail.com>
2023-01-08 23:51:17 -0500

    nav

--- src/components/footerbar.js
+++ src/components/footerbar.js
@@ -8,21 +8,21 @@ function Footerbar() {
       <div className="container mx-auto flex justify-between items-center sm:flex-col sm:gap-8">
         <div className="flex gap-x-2 caption">
           <a
-            className="py-1 px-2 rounded-md bg-blue-100 text-blue-500 font-medium"
+            className="py-2 px-3 rounded-md bg-slate-100 text-xs font-medium text-slate-600 active:bg-slate-300 active:text-slate-900 hover:bg-slate-200 hover:text-slate-700"
             target="_blank"
             href="https://www.dropbox.com/s/q695q022e75l8kd/Resume-Jan2023.pdf?dl=0"
           >
             CV
           </a>
           <a
-            className="py-1 px-2 rounded-md bg-blue-100 text-blue-500 font-medium"
+            className="py-2 px-3 rounded-md bg-slate-100 text-xs font-medium text-slate-600 active:bg-slate-300 active:text-slate-900 hover:bg-slate-200 hover:text-slate-700"
             target="_blank"
             href="https://www.linkedin.com/in/karenamistry/"
           >
             LinkedIn
           </a>
           <a
-            className="py-1 px-2 rounded-md bg-blue-100 text-blue-500 font-medium"
+            className="py-2 px-3 rounded-md bg-slate-100 text-xs font-medium text-slate-600 active:bg-slate-300 active:text-slate-900 hover:bg-slate-200 hover:text-slate-700"
             target="_blank"
             href="mailto:k.mistry1898@gmail.com"
           >
@@ -30,11 +30,11 @@ function Footerbar() {
           </a>
         </div>
 
-        <div className="p flex gap-12">
-          <div className="hover:text-blue-500">
+        <div className="flex gap-x-2 caption">
+          <div className="py-2 px-3 rounded-md bg-blue-100 text-xs font-medium text-blue-600 active:bg-blue-300 active:text-blue-900 hover:bg-blue-200 hover:text-blue-700">
             <NavLink to="/">Home</NavLink>
           </div>
-          <div className="hover:text-blue-500">
+          <div className="py-2 px-3 rounded-md bg-blue-100 text-xs font-medium text-blue-600 active:bg-blue-300 active:text-blue-900 hover:bg-blue-200 hover:text-blue-700">
             <NavLink to="/about">About</NavLink>
           </div>
         </div>
