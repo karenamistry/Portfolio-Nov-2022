93600c28879cc86d3b5417fc3d0ea77f1c5c7d44
Karena <k.mistry1898@gmail.com>
2023-01-25 14:24:36 -0500

    hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,16 +6,16 @@ function Hero() {
   return (
     <div className="container w-screen h-[100vh] flex flex-col justify-center items-start gap-6 md:flex-row md:justify-center md:items-center">
       <img
-        className="w-40 md:w-40 rounded-[2rem] shadow-[0_35px_60px_-15px_rgba(59,130,246,0.25)] m-4"
+        className="w-40 md:w-40 rounded-[2rem] shadow-[0_35px_60px_-15px_rgba(59,130,246,0.25)] m-4 border-[0.5px] border-gray-50"
         src={Profile}
         alt="me!"
       />
       <div className="flex flex-col gap-y-4">
         <div>
-          <div className="h2">Hi, I'm Karena Mistry</div>
-          <div className="h5 max-w-lg">
-            I'm a product designer based in Canada. I'm passionate about
-            creating thoughtful experiences and carefully crafted interfaces.
+          <div className="h3 max-w-xl">
+            <span className="text-blue-600">I'm Karena.</span> I'm a product
+            designer based in Canada. I'm passionate about creating thoughtful
+            experiences and carefully crafted interfaces.
           </div>
         </div>
         <div>
