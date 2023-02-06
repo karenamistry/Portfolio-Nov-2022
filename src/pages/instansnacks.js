ab5bfc04e728ed0a653b45cc9da1bc82d2550bdd
Karena <k.mistry1898@gmail.com>
2023-02-06 13:44:30 -0500

    insta

--- src/pages/instansnacks.js
+++ src/pages/instansnacks.js
@@ -13,6 +13,7 @@ import Pretzel from "../images/instasnacks/Instasnacks_cute_3.jpg";
 import Poppop from "../images/instasnacks/Instasnacks_cyclone.jpg";
 import Lolly from "../images/instasnacks/Instasnacks_cute_1.jpg";
 import Choco from "../images/instasnacks/choco.png";
+import Process from "../images/instasnacks/process-a.png";
 
 import Video from "../components/vimeo.js";
 
@@ -28,11 +29,13 @@ function Instansnacks() {
             <div className="flex flex-col gap-y-8 max-w-4xl">
               <div className="h3">
                 Instasnacks delivers corner store products and specialty snacks
-                using food delivery services. I managed the social media
-                strategy and creation of daily content for Instagram, Facebook,
-                and TikTok. I was required to work quickly and efficiently in to
-                optimize interactions, while adhering to social media
-                algorithms.
+                using food delivery services. I managed their social media for
+                Instagram, Facebook, and TikTok. I{" "}
+                <span className="text-black font-medium">
+                  communicated with customers to conduct user research and
+                  create marketing strategy's
+                </span>{" "}
+                to increase engagement.
               </div>
             </div>
             <div className="flex gap-x-6">
@@ -79,7 +82,33 @@ function Instansnacks() {
             Instansnacks sells through delivery applications such as UberEats,
             Door Dash and Skip the Dishes. They have 4 locations across the
             Ottawa & Gatineau area. Social media is the primary source of online
-            exposure to promote their business.
+            exposure to promote their business. I published daily illustrated
+            and animated content, and conducted user research with the goal of
+            increasing engagement to advertise their online store.
+          </div>
+        </div>
+
+        <div className="h1 md:px-12">Process</div>
+
+        <div className="flex flex-col max-w-4xl gap-y-2 md:px-12">
+          <div className="h2">Getting Input from Users</div>
+          <div className="h3">
+            An important part of my job was interacting with customers. To
+            gather data on what users were responding to, I parsed past comments
+            and past posts that garnered the highest impressions. I got in
+            contact with top influencers (with 25K+ followers), to see what they
+            though about our brand, and the potential of a collaboration.
+          </div>
+        </div>
+
+        <div className="flex flex-col max-w-4xl gap-y-2 md:px-12">
+          <div className="h2">Get Snackin' Campaign</div>
+          <div className="h3">
+            Using this information, I was able to know what I wanted to
+            accomplish with my posts. People enjoyed that our brand that was
+            young, tech-savy and fun. I did some sketching for potential
+            directions and we decided to move forward with the Get Snackin'
+            Posts.
           </div>
         </div>
 
@@ -88,17 +117,29 @@ function Instansnacks() {
           <div className="h3">
             I was able to gain a steady increase in engagement from previous
             content posted, peaking at a 46% increase in engagement from
-            previous content. The more significant difference was the increase
-            in interactions on reel content compared to regular posts. Reel
-            content had at the very least 4 times as many interactions than the
-            regular posts. Regular posts received fewer interactions compared to
-            Reels, but they did really well to entice profile visits, all
-            illustrated posts were among the top posts to entice profile visits
-            at the end of each month they were posted.
+            previous content. This was an impression increase by the hundreds.
+          </div>
+          <div className="h3">
+            The increase in impressions on illustrated posts was a clear enough
+            distinction, so we decided to apply these illustrated styles to
+            Instagram Reel content. This was successful, with illustrated motion
+            content having at the very least 4 times as many impression than the
+            illustrated posts. Regular posts received fewer impressions compared
+            to Reels, but they did really well to entice profile visits and
+            engagement, all illustrated posts were among the top posts to entice
+            profile visits at the end of each month they were posted.
           </div>
         </div>
 
-        <div className="h1 md:px-12">Select Work</div>
+        <div className="w-full h-full flex flex-col md:flex-row gap-x-6 gap-y-4">
+          <img
+            className="object-cover mx-auto h-full w-5/6 rounded-xl"
+            src={Process}
+            alt="Image"
+          />
+        </div>
+
+        <div className="h1 md:px-12">Select Posts</div>
 
         <div className="">
           <Video />
@@ -161,19 +202,6 @@ function Instansnacks() {
             <img
               className="object-cover mx-auto h-full w-full md:w-1/2 rounded-xl"
               src={Can}
-              alt="Image"
-            />
-          </div>
-
-          <div className="w-full h-full flex flex-col md:flex-row gap-x-6 gap-y-4">
-            <img
-              className="object-cover mx-auto h-full w-full md:w-1/2 rounded-xl"
-              src={Contest}
-              alt="Image"
-            />
-            <img
-              className="object-cover mx-auto h-full w-full md:w-1/2 rounded-xl"
-              src={Movie}
               alt="Image"
             />
           </div>
