203ee96bc6b1ed0f94f559b8d3502fcbd95f22ec
Karena <k.mistry1898@gmail.com>
2023-02-13 14:29:01 -0500

    this

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,55 +7,57 @@ import Collage from "../images/about/me-collage.png";
 function About() {
   return (
     <div className="w-screen h-screen bg-zinc-900">
-      <div className="py-[20vh] bg-zinc-900">
-        <div className="container flex flex-col lg:flex-row justify-center">
-          <div className="flex flex-col  gap-y-6 max-w-xl">
-            <div className="h2">Hello! I'm Karena.</div>
-            <div className="h3">
-              I'm a designer with a BA in design from York University/Sheridan
-              College, Joint program. I'm currently living in Ottawa, Canada. I
-              call myself a designer of all kinds of things, because i'm a very
-              versatile designer with experience in print and publishing, social
-              media marketing, motion, and UR/UI/UX design. I believe in
-              creating intentional & data-driven design.
-            </div>
-            <div className="h3">
-              I've worked on small but impactful teams, sometimes as the only
-              design voice. I've worked freelance for clients like: Canadian
-              Tire, Upside Realty and Trustee Alysha Aziz. I'm currently working
-              as a freelance product designer for Dynamic CXS.
-            </div>
-
-            <div className="h3"></div>
-            <div className="flex flex-col gap-y-2">
-              <div className="h4">I'm always down to chat!</div>
+      <div className="md:max-w-7xl mx-auto">
+        <div className="py-[20vh] bg-zinc-900">
+          <div className="container flex flex-col lg:flex-row justify-center">
+            <div className="flex flex-col  gap-y-6 max-w-xl">
+              <div className="h2">Hello! I'm Karena.</div>
               <div className="h3">
-                I check my email most often, at{" "}
-                <a
-                  target="_blank"
-                  href="mailto:k.mistry1898@gmail.com"
-                  className=" transition duration-150 ease-out hover:ease-in text-zinc-200"
-                >
-                  k.mistry1898@gmail.com
-                </a>
-                . You can visit my{" "}
-                <a
-                  className=" transition duration-150 ease-out hover:ease-in text-zinc-200"
-                  target="_blank"
-                  href="https://www.linkedin.com/in/karenamistry/"
-                >
-                  LinkedIn{" "}
-                </a>
-                to see my experience.
+                I'm a designer with a BA in design from York University/Sheridan
+                College, Joint program. I'm currently living in Ottawa, Canada.
+                I call myself a designer of all kinds of things, because i'm a
+                very versatile designer with experience in print and publishing,
+                social media marketing, motion, and UR/UI/UX design. I believe
+                in creating intentional & data-driven design.
+              </div>
+              <div className="h3">
+                I've worked on small but impactful teams, sometimes as the only
+                design voice. I've worked freelance for clients like: Canadian
+                Tire, Upside Realty and Trustee Alysha Aziz. I'm currently
+                working as a freelance product designer for Dynamic CXS.
+              </div>
+
+              <div className="h3"></div>
+              <div className="flex flex-col gap-y-2">
+                <div className="h4">I'm always down to chat!</div>
+                <div className="h3">
+                  I check my email most often, at{" "}
+                  <a
+                    target="_blank"
+                    href="mailto:k.mistry1898@gmail.com"
+                    className=" transition duration-150 ease-out hover:ease-in text-zinc-200"
+                  >
+                    k.mistry1898@gmail.com
+                  </a>
+                  . You can visit my{" "}
+                  <a
+                    className=" transition duration-150 ease-out hover:ease-in text-zinc-200"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/karenamistry/"
+                  >
+                    LinkedIn{" "}
+                  </a>
+                  to see my experience.
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-full h-full rounded-sm">
-            <img
-              className="object-cover object-top w-[20rem] lg:w-[17rem]  mx-auto pt-12 lg:pt-0 rounded-[15px]"
-              src={Standing}
-              alt="me!"
-            />
+            <div className="w-full h-full rounded-sm">
+              <img
+                className="object-cover object-top w-[20rem] lg:w-[17rem]  mx-auto pt-12 lg:pt-0 rounded-[15px]"
+                src={Standing}
+                alt="me!"
+              />
+            </div>
           </div>
         </div>
       </div>
